b491964f516a1653a3ec3b87f4e87bb7310b3431
Krishan Sapkota <krishansapkota@gmail.com>
2026-09-08 09:17:50 -0400

    adding middleware

--- backend/main.py
+++ backend/main.py
@@ -5,8 +5,18 @@ Wires together all routers.
 
 from fastapi import FastAPI
 from routers import representatives, issues, users
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(representatives.router)
 app.include_router(issues.router)
 app.include_router(users.router)
